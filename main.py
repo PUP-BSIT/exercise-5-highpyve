1d527dbf4cae68df5846ed4d22d4bb3afa6c2792
Mikaela Joy Bartolome <mikaela.bartolome13@gmail.com>
2025-03-18 22:40:51 +0800

    Add Mika's Questions

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 score_counter = 0
 
-print("1. (Kristoffer Anipan) What is my favorite snack? ")
-print("a. Jumpee")
-print("b. Loaded ")
-print("c. Pillows")
-print("d. Piattos")
+print("Welcome to HighPYve Quiz!")
+print("Answer the following questions by choosing the correct letter.")
+
+print("\n1. (Kristoffer Anipan) What is my favorite snack? ")
+print("a. Jumpee                c. Pillows")           
+print("b. Loaded                d. Piattos")
 user_choice = input("Enter your answer: ").lower()
 
 if user_choice == 'a': 
@@ -14,10 +15,8 @@ else:
     print(f"{user_choice} is incorrect. The correct answer is a.")
 
 print("\n2. (Kristoffer Anipan) What is my age?")
-print("a. 20")
-print("b. 19 ")
-print("c. 21")
-print("d. 18")
+print("a. 20                   c. 21") 
+print("b. 19                   d. 18")
 user_choice = input("Enter your answer: ").lower()
 
 if user_choice == 'a': 
@@ -27,10 +26,8 @@ else:
     print(f"{user_choice} is incorrect. The correct answer is a.")
 
 print("\n3. (Ma. Rose L. Tolentino) Which naming convention is used in Python?")
-print("a. Pascal Case")
-print("b. Snake Case")
-print("c. Camel Case")
-print("d. Eagle Case")
+print("a. Pascal Case           c. Camel Case")
+print("b. Snake Case            d. Eagle Case")
 user_choice = input("Enter your answer: ").lower()
 
 if user_choice == 'b': 
@@ -39,12 +36,9 @@ if user_choice == 'b':
 else: 
     print(f"{user_choice} is incorrect. The correct answer is b.")
 
-print("\n4. (Ma. Rose L. Tolentino) ")
-print("What data type is the following?")
-print("a. Boolean")
-print("b. String")
-print("c. Integer")
-print("d. Float")
+print("\n4. (Ma. Rose L. Tolentino) What data type is the following?")
+print("a. Boolean             c. Integer")
+print("b. String              d. Float")
 user_choice = input("Enter your answer: ").lower()
 
 if user_choice == 'b': 
@@ -52,5 +46,27 @@ if user_choice == 'b':
     score_counter += 1
 else: 
     print(f"{user_choice} is incorrect. The correct answer is b.")
-    
+
+print("\n5. (Mikaela Bartolome) Who is my favorite Sanrio Character")
+print("a. Hello Kitty          c. Keroppi")
+print("b. My Melody            d. Kuromi")
+user_choice = input("Enter your answer: ").lower()
+
+if user_choice == 'b': 
+    print("You got the correct answer. Congratulations.")
+    score_counter += 1
+else: 
+    print(f"{user_choice} is incorrect. The correct answer is b.")
+
+print("\n6. (Mikaela Bartolome) Where is the location of the first F1 Grand Prix track in 2025?")
+print("a. Monaco              c. Australia")
+print("b. China               d. Abu Dhabi")
+user_choice = input("Enter your answer: ").lower()
+
+if user_choice == 'c': 
+    print("You got the correct answer. Congratulations.")
+    score_counter += 1
+else: 
+    print(f"{user_choice} is incorrect. The correct answer is c.")
+
 print(f"\nYour score is {score_counter}/10")
